dabd4fcd632057861bcb487a6403cf0de2cb94cc
darshana_123 <darshanabiradar23@gmail.com>
2023-11-02 16:30:14 +0530

    07_datesinJS

---
+++ 01_Basics/07_Dates_in_JS.js
@@ -0,0 +1,21 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString);
+console.log(myDate.toLocaleString);
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023,0,23)
+console.log(myCreatedDate.toDateString());
+let myCreatedDate1 = new Date(2023,0,23,5,3)
+let myCreatedDate2 = new Date("2023-01-14")
+console.log(myCreatedDate1.toLocaleString());
+console.log(myCreatedDate2.toLocaleString());
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Date.now());
+console.log(Math.floor(Date.now()/1000));
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
